58591ef669561e349fade61fa7c4eefb3759b18a
Jonathan <poisson.jonathan777@gmail.com>
2016-10-04 20:40:39 -0400

    Added basic error handling

--- chapter1/server.js
+++ chapter1/server.js
@@ -5,7 +5,11 @@ var app = express();
 var jsonData = {count: 12, message: 'hey'};
 
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname + '/index.html'));
+  res.sendFile(path.join(__dirname + '/index.html'), function(err) {
+    if (err) {
+      res.status(500).send(err);
+    }
+  });
 });
 
 app.get('/data', function(req, res) {
