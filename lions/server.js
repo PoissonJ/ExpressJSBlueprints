4e0d6f890f0e64a588f4848ff1c3365539d61be3
Jonathan <poisson.jonathan777@gmail.com>
2016-10-05 21:12:39 -0400

    Moved routes into subfile

--- lions/server.js
+++ lions/server.js
@@ -1,79 +1,16 @@
-var path = require('path');
 var express = require('express');
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
-var _ = require('lodash');
 var app = express();
 
+var lionRouter = require('./lions');
 
 app.use(express.static('.'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(morgan());
 
-var lions = [];
-var id = 0;
-
-var updateId = function(req, res, next) {
-  if (!req.body.id) {
-    id++;
-    req.body.id = id + '';
-  }
-  next();
-}
-
-app.param('id', function(req, res, next, id) {
-  var lion = _.find(lions, {id: req.params.id});
-
-  if (lion) {
-    req.lion = lion;
-    next();
-  } else {
-    res.status(500).send();
-  }
-});
-
-app.get('/lions', function(req, res) {
-  res.send(lions);
-});
-
-app.get('/lions/:id', function(req, res) {
-  res.json(req.lion || {});
-});
-
-app.post('/lions', updateId, function(req, res) {
-  var lion = req.body;
-
-  lions.push(lion);
-
-  res.json(lion);
-});
-
-app.put('/lions/:id', function(req, res) {
-  var update = req.body;
-  if (update.id) {
-    delete update.id
-  }
-
-  var lion = _.findIndex(lions, {id: req.params.id});
-  if (!lions[lion]) {
-    res.status(500).send();
-  } else {
-    var updatedLion = _.assign(lions[lion], update);
-    res.json(updatedLion);
-  }
-});
-
-app.delete('/lions/:id', function(req, res) {
-  var lion = _.findIndex(lions, {id: req.params.id});
-  if (!lions[lion]) {
-    res.status(500).send();
-  } else {
-    var deletedLion = lions[lion]
-    lions.splice(lion, 1);
-    res.json(deletedLion);
-  }
-});
+app.use('/lions', lionRouter);
 
 
 /**
@@ -84,7 +21,6 @@ app.use(function(err, req, res, next) {
     res.status(500).send(err);
   }
 });
-
 
 app.listen(3000);
 console.log('Listening on port 3000');
